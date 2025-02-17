ad9a86e75f328ab50a1af3489109c3bb8bd48b1f
Anatoli Trabko <toliktrebko@gmail.com>
2025-02-17 13:00:14 +0300

    faet: add footer mobile, header mobile, partner mobile

--- src/components/pages/services/partner/Partner.jsx
+++ src/components/pages/services/partner/Partner.jsx
@@ -1,61 +1,146 @@
 import "./Partner.scss";
 import travels from "@assets/partner/travelers.jpg";
 import message from "@assets/partner/message.svg";
+import forest from "@assets/partner/forest.png";
+import { useState } from "react";
 
 const Partner = () => {
+  const [regionCode, setRegionCode] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const regionCodes = [
+    { code: "(+1)", country: "USA", flag: "https://flagcdn.com/us.svg" },
+    { code: "(+44)", country: "UK", flag: "https://flagcdn.com/gb.svg" },
+    { code: "(+7)", country: "Russia", flag: "https://flagcdn.com/ru.svg" },
+  ];
+
+  const handleRegionChange = (event) => {
+    setRegionCode(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const selectedRegion = regionCodes.find(
+    (region) => region.code === regionCode
+  );
+  const flagUrl = selectedRegion ? selectedRegion.flag : "";
+
   return (
     <section className="partner">
-      <div className="partner__container container">
-        <img src={travels} alt="" loading="eager" aria-hidden="true" />
-
+      <div className="partner__container ">
+        <img
+          src={travels}
+          className="partner__image-travels"
+          alt=""
+          loading="eager"
+          aria-hidden="true"
+        />
+        <img
+          src={forest}
+          className="partner__image-forest"
+          alt=""
+          loading="eager"
+          aria-hidden="true"
+        />
         <div className="partner__wrapper-form">
           <h2 className="partner__title">Partner With Us</h2>
           <form className="partner__form" action="">
-            <div className="partner__input-wrapper">
-              <input
-                type="text"
-                placeholder="Enter Your First Name"
-                className="partner__input"
-              />
-              <input
-                type="text"
-                placeholder="Enter Your Last Name"
-                className="partner__input"
-              />
-            </div>
-            <div className="partner__input-wrapper">
-              <input
-                type="email"
-                placeholder="Enter Your Email"
-                className="partner__input"
-              />
-              <input type="tel" className="partner__input" />
-            </div>
-            <div className="partner__input-wrapper">
+            <div className="partner__container-form">
+              <div className="partner__input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Enter Your First Name"
+                  className="partner__input"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter Your Last Name"
+                  className="partner__input"
+                />
+              </div>
+              <div className="partner__input-wrapper">
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="partner__input"
+                />
+                <div className="partner__telephone-wrapper">
+                  <div className="partner__code-wrapper">
+                    <select
+                      className="partner__select-code"
+                      value={regionCode}
+                      onChange={handleRegionChange}
+                    >
+                      {regionCodes.map((region) => (
+                        <option key={region.code} value={region.code}>
+                          {region.code}
+                        </option>
+                      ))}
+                    </select>
+
+                    <img
+                      className="partner__image-flag"
+                      src={flagUrl || regionCodes[0].flag}
+                      alt=""
+                      loading="lazy"
+                      aria-hidden="true"
+                    />
+                  </div>
+
+                  <input
+                    type="tel"
+                    value={phoneNumber}
+                    onChange={handlePhoneNumberChange}
+                    placeholder="000000000"
+                    className="partner__input telephone-input"
+                  />
+                </div>
+              </div>
+
               <input
                 type="text"
                 placeholder="Enter Your Businesss Name"
                 className="partner__input"
               />
+
+              <select
+                className="partner__select"
+                name="Choose Your amount of Employees"
+                id=""
+              >
+                <option value="null">Choose Your amount of Employees</option>
+                <option value="Sad music is playing">
+                  "Sad music is playing"
+                </option>
+                <option value="Lorem lorem lorem">Lorem lorem lorem</option>
+                <option value="Lorem inpus? inpum lorem ohh...">
+                  Lorem inpus? inpum lorem ohh...
+                </option>
+                <option value="No, not now lorem inpsum... Just Not now...">
+                  No, not now lorem inpsum... Just Not now...
+                </option>
+                <option value="What are we here for, lorem ipsum?">
+                  What are we here for, lorem ipsum?
+                </option>
+                <option value="Loreeeeeeem ipsuuuuuummmmm">
+                  Loreeeeeeem ipsuuuuuummmmm
+                </option>
+              </select>
             </div>
-            <select
-              className="partner__select"
-              name="Choose Your amount of Employees"
-              id=""
-            >
-              <option value="null">Choose Your amount of Employees</option>
-              <option value="null">"Sad music is playing"</option>
-              <option value="null">Lorem lorem lorem</option>
-              <option value="null">Lorem inpus? inpum lorem ohh...</option>
-              <option value="null">
-                No, not now lorem inpsum... Just Not now...
-              </option>
-              <option value="null">What are we here for, lorem ipsum? </option>
-              <option value="null">Loreeeeeeem ipsuuuuuummmmm</option>
-            </select>
             <div className="partner__wrapper-button">
-              <button type="send" className="partner__button button button-violet">
-                <img src={message} alt="" loading="eager" aria-hidden="true" />
+              <button
+                type="send"
+                className="partner__button button button-violet"
+              >
+                <img
+                  src={message}
+                  className="partner__image-message"
+                  alt=""
+                  loading="eager"
+                  aria-hidden="true"
+                />
                 <span>Contact Us</span>
               </button>
               <p className="partner__sub-text">
--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,6 +1,9 @@
 import "./Header.scss";
 import logo from "@assets/logo.svg";
+import iconMenu from "@assets/header/iconMenu.svg";
+
 import border from "@assets/header/border.svg";
+import { useState } from "react";
 import { navigation, social } from "./data";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
@@ -8,7 +11,9 @@ import { useLocation } from "react-router";
 const Header = () => {
   const location = useLocation();
   const isActive = (path) => location.pathname === path;
-  
+
+  const [activeMenu, setActiveMenu] = useState(false);
+
   return (
     <header className="header">
       <div className="header__container container">
@@ -19,40 +24,87 @@ const Header = () => {
           loading="eager"
           aria-hidden="true"
         />
-        <nav>
-          <ul className="header__list">
-            {navigation.map((item, i) => (
-              <li key={i} className="header__list-item">
-                <Link to={item.link} className="header__item-link">
-                  {item.text}
-                </Link>
-                {isActive(item.link) ? (
-                  <img src={border} loading="eager" aria-hidden="true" />
-                ) : (
-                  ""
-                )}
+
+        <div className="header__block">
+          <nav>
+            <ul className="header__list">
+              {navigation.map((item, i) => (
+                <li key={i} className="header__list-item">
+                  <Link to={item.link} className="header__item-link">
+                    {item.text}
+                  </Link>
+                  {isActive(item.link) ? (
+                    <img src={border} loading="eager" aria-hidden="true" />
+                  ) : (
+                    ""
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className="header__block">
+          <ul className="header__list-social">
+            {social.map((item, i) => (
+              <li key={i}>
+                <a href={item.link} className="header__link-social">
+                  <img
+                    className="header__image-social"
+                    src={item.image}
+                    alt={item.alt}
+                    loading="eager"
+                    aria-hidden="true"
+                  />
+                </a>
               </li>
             ))}
-          </ul>
-        </nav>
-        <ul className="header__list-social">
-          {social.map((item, i) => (
-            <li key={i}>
-              <a href={item.link} className="header__link-social">
-                <img
-                  className="header__image-social"
-                  src={item.image}
-                  alt={item.alt}
-                  loading="eager"
-                  aria-hidden="true"
-                />
-              </a>
+            <li>
+              <button className="header__button">Сontact us</button>
             </li>
-          ))}
-          <li>
-            <button className="header__button">Сontact us</button>
-          </li>
-        </ul>
+          </ul>
+        </div>
+
+        <button
+          className="header__button-menu"
+          onClick={() => setActiveMenu((prevState) => !prevState)}
+          type="button"
+        >
+          <img src={iconMenu} alt="" />
+        </button>
+
+        {activeMenu && (
+          <div className="header__menu">
+            <nav>
+              <ul className="header__list">
+                {navigation.map((item, i) => (
+                  <li key={i} className="header__list-item">
+                    <Link to={item.link} className="header__item-link">
+                      {item.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>{" "}
+            <ul className="header__list-social">
+              {social.map((item, i) => (
+                <li key={i}>
+                  <a href={item.link} className="header__link-social">
+                    <img
+                      className="header__image-social"
+                      src={item.image}
+                      alt={item.alt}
+                      loading="eager"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </li>
+              ))}
+              <li>
+                <button className="header__button">Сontact us</button>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </header>
   );
